0fc6c93850ed1e32c3c9cc7379c9c89f27b8401c
tesla1618 <rajiebbb@gmail.com>
2026-03-30 21:07:03 +0600

    cdn setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 // next.config.js
 module.exports = {
   allowedDevOrigins: ["192.168.0.113"], // this is my local IP address, and I have no problem with it being public. You can even publish it to a tech news website calling me a "dumbass".
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.icami.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
